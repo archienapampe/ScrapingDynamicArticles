151fa3a41f407d5ec9a0a8c2594ebb666ba5d4a7
Artur Husti <artur.husty12@gmail.com>
2020-06-22 17:10:33 +0300

    create README

--- scraping_project/models.py
+++ scraping_project/models.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 
 
 def db_connect():
-    return create_engine(get_project_settings().get("CONNECTION_STRING"))
+    return create_engine(get_project_settings().get('CONNECTION_STRING'))
 
 
 def create_table(engine):
@@ -23,25 +23,25 @@ author_article = Table('author_article', Base.metadata,
 
 
 class Article(Base):
-    __tablename__ = "article"
+    __tablename__ = 'article'
 
     id = Column(Integer, primary_key=True)
     title = Column('title', Text())
     published = Column('published', Date)
     category_id = Column(Integer, ForeignKey('category.id'))
-    authors = relationship('Author', secondary='author_article', lazy='dynamic', backref="article") 
+    authors = relationship('Author', secondary='author_article', lazy='dynamic', backref='article') 
           
 
 class Author(Base):
-    __tablename__ = "author"
+    __tablename__ = 'author'
 
     id = Column(Integer, primary_key=True)
     name = Column('name', String(50), unique=True)
-    articles = relationship('Article', secondary='author_article', lazy='dynamic', backref="author") 
+    articles = relationship('Article', secondary='author_article', lazy='dynamic', backref='author') 
           
    
 class Category(Base):
-    __tablename__ = "category"
+    __tablename__ = 'category'
 
     id = Column(Integer, primary_key=True)
     name = Column('name', String(30), unique=True)
